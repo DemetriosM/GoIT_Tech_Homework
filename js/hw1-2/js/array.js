a19c859da0163958363fb725de9e9d9e6cd196d2
Дмитрий <dmdonetsk@outlook.com>
2016-03-10 23:28:30 +0200

    Добавил д.р. по js №1-2

---
+++ js/hw1-2/js/array.js
@@ -0,0 +1,22 @@
+﻿var names = [];
+var userName = '';
+
+alert('Введите 5 имен');
+for (var i = 1; i <= 5; i++) {
+  var str = 'Введите любое имя ' + i;
+  names[i] = prompt(str, '').toLowerCase();
+}
+
+userName = prompt('Введите ваше имя', '');
+if (authentication(userName)) {
+  var str = userName + ', вы успешно вошли';
+  alert(str);
+}else {
+  alert('В доступе отказано :)');
+}
+
+function authentication(userName) {
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] == userName.toLowerCase()) return true;
+  }
+}
---
+++ js/hw1-2/js/pow.js
@@ -0,0 +1,21 @@
+﻿alert('Возведение числа в степень');
+
+var x = prompt('Введите число x=', '');
+var n = prompt('Введите степень (целочисленное число больше нуля) n=', '');
+var result = x;
+
+if (!isNaN(x) & !isNaN(n) & Math.round(n) == n & n > 0) {
+  result = pow(x, n);
+  console.log('x =', x);
+  console.log('n =', n);
+  console.log('x в степени n =', result);
+} else {
+  console.log('pow - Некорректно введены данные');
+}
+
+function pow(x, n) {
+  for (var i = 0; i < n-1; i++) {
+      result *= x;
+  }
+  return result;
+}
