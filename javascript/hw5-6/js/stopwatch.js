5b8b22f3a582cd19b3d1e954207674a223a881dc
DemetriosM <dmdonetsk@outlook.com>
2016-03-28 09:55:56 +0300

    Переименовал методы в др 5-6

--- javascript/hw5-6/js/stopwatch.js
+++ javascript/hw5-6/js/stopwatch.js
@@ -1,5 +1,5 @@
 ﻿/*-----------------------------------Timer------------------------------------------*/
-function Timer() {
+function CreateTimer() {
 
   var panel = {
     status: 'Start',
@@ -12,29 +12,29 @@ function Timer() {
   var stopsList = [];
   var flagStartStop = true;
   var timerId;
-  var timerPrint = new TimerPrint(panel);
-  timerPrint.printReset();
+  var displayTimer = new DisplayTimer(panel);
+  displayTimer.printReset();
 
-  var timerRuning = function () {
-    timerPrint.printMsc();
+  var runTimer = function () {
+    displayTimer.printMsc();
     panel.msc +=43;
     if (panel.msc >= 1000) {
       panel.msc = 32;
       panel.sc++;
-      timerPrint.printSc();
+      displayTimer.printSc();
     }
     if (panel.sc == 60) {
       panel.sc = 0;
       panel.mm++;
-      timerPrint.printMm();
+      displayTimer.printMm();
     }
     if (panel.mm == 60) {
       panel.mm = 0;
       panel.hh++;
-      timerPrint.printHh();
+      displayTimer.printHh();
     }
     if (panel.hh == 24) {
-      timerPrint.printReset();
+      displayTimer.printReset();
       panel.hh = 0;
       panel.mm = 0;
       panel.sc = 0;
@@ -48,7 +48,7 @@ function Timer() {
       this.copy[key] = panel[key];
     }
   }
-  function lastClonePanel() {
+  function getLastClonePanel() {
     for (var key in panel) {
       lastPanel[key] = panel[key];
     }
@@ -71,36 +71,36 @@ function Timer() {
     copy.msc = sumCopy % 1000;
     copy.status = status;
     stopsList.push(copy);
-    lastClonePanel();
+    getLastClonePanel();
   }
-            /*---action---*/
-  this.timerStartStop = function () {
+  /*---event handlers click (start/stop, split, reset)---*/
+  this.startStopTimer = function () {
     if (flagStartStop) {
       flagStartStop = false;
-      timerId = setInterval(timerRuning, 43);
+      timerId = setInterval(runTimer, 43);
       panel.status = 'Stop';
     } else {
       flagStartStop = true;
       clearInterval(timerId);
       var copyPanel = new ClonePanel().copy;
       pushSubtractStopItem(copyPanel, 'Stop');
-      timerPrint.printSplitItem(stopsList[stopsList.length - 1]);
+      displayTimer.printSplitItem(stopsList[stopsList.length - 1]);
       panel.status = 'Start';
     }
-    timerPrint.printButtonStartStop(panel.status);
+    displayTimer.printButtonStartStop(panel.status);
   }
 
-  this.timerSplit = function () {
+  this.splitTimer = function () {
     if (!flagStartStop) {
       var copyPanel = new ClonePanel().copy;
       pushSubtractStopItem(copyPanel, 'Split');
-      timerPrint.printSplitItem(stopsList[stopsList.length - 1]);
+      displayTimer.printSplitItem(stopsList[stopsList.length - 1]);
     }
   }
 
-  this.timerReset = function () {
+  this.resetTimer = function () {
     clearInterval(timerId);
-    timerPrint.printReset();
+    displayTimer.printReset();
     flagStartStop = true;
     stopsList = [];
     panel.status = 'Start';
@@ -111,8 +111,8 @@ function Timer() {
   }
 }
 
-/*-----------------------------------TimerPrint------------------------------------------*/
-function TimerPrint(panel) {
+/*-----------------------------------DisplayTimer------------------------------------------*/
+function DisplayTimer(panel) {
   this.printMsc = function() {
     if (panel.msc < 10) {
       document.getElementsByClassName('msc')[0].innerHTML = '.00' + panel.msc;
@@ -129,7 +129,7 @@ function TimerPrint(panel) {
     if (panel.sc < 10) {
       document.getElementsByClassName('sc')[0].innerHTML = ':0' + panel.sc;
     } else {
-      if (panel.sc == 60) {
+      if (panel.sc === 60) {
         document.getElementsByClassName('sc')[0].innerHTML = ':00';
       } else {
         document.getElementsByClassName('sc')[0].innerHTML = ':' + panel.sc;
@@ -141,7 +141,7 @@ function TimerPrint(panel) {
     if (panel.mm < 10) {
       document.getElementsByClassName('mm')[0].innerHTML = ':0' + panel.mm;
     } else {
-      if (panel.mm == 60) {
+      if (panel.mm === 60) {
         document.getElementsByClassName('mm')[0].innerHTML = ':00';
       } else {
         document.getElementsByClassName('mm')[0].innerHTML = ':' + panel.mm;
@@ -168,8 +168,8 @@ function TimerPrint(panel) {
   this.printSplitItem = function (stopListItem) {
     var splitItem = document.createElement('li');
     splitItem.classList.add('list-group-item');
-    if (stopListItem.status == 'Split') splitItem.classList.add('list-group-item-info');
-    if (stopListItem.status == 'Stop') splitItem.classList.add('list-group-item-danger');
+    if (stopListItem.status === 'Split') splitItem.classList.add('list-group-item-info');
+    if (stopListItem.status === 'Stop') splitItem.classList.add('list-group-item-danger');
     stopListItem = convertToString(stopListItem);
     splitItem.innerHTML = stopListItem.status + stopListItem.hh + stopListItem.mm + stopListItem.sc + stopListItem.msc;
     document.getElementsByClassName('list')[0].appendChild(splitItem);
@@ -183,10 +183,10 @@ function TimerPrint(panel) {
     if (elem.hh < 10) elem.hh = ' 0' + elem.hh;
     if (elem.mm < 10) elem.mm = ':0' + elem.mm;
     if (elem.mm > 10) elem.mm = ':' + elem.mm;
-    if (elem.mm == 60) elem.mm = ':00';
+    if (elem.mm === 60) elem.mm = ':00';
     if (elem.sc < 10) elem.sc = ':0' + elem.sc;
     if (elem.sc > 10) elem.sc = ':' + elem.sc;
-    if (elem.sc == 60) elem.sc = ':00';
+    if (elem.sc === 60) elem.sc = ':00';
     if (elem.msc < 10) elem.msc = '.00' + elem.msc;
     if (elem.msc >= 10 & elem.msc < 100) elem.msc = '.0' + elem.msc;
     if (elem.msc >= 100) elem.msc = '.' + elem.msc;
@@ -196,7 +196,8 @@ function TimerPrint(panel) {
 }
 /*-----------------------------------------------------------------------------*/
 
-var timer = new Timer();
-document.getElementsByClassName('start')[0].addEventListener('click', timer.timerStartStop);
-document.getElementsByClassName('split')[0].addEventListener('click', timer.timerSplit);
-document.getElementsByClassName('reset')[0].addEventListener('click', timer.timerReset);
+
+var timer = new CreateTimer();
+document.getElementsByClassName('start')[0].addEventListener('click', timer.startStopTimer);
+document.getElementsByClassName('split')[0].addEventListener('click', timer.splitTimer);
+document.getElementsByClassName('reset')[0].addEventListener('click', timer.resetTimer);
