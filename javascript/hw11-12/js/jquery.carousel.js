8be693f93be3880c26dcf63aa1543890ab83c744
DemetriosM <dmdonetsk@outlook.com>
2016-04-17 19:14:04 +0300

    добавил в js др11-12

---
+++ javascript/hw11-12/js/jquery.carousel.js
@@ -0,0 +1,50 @@
+﻿(function ($) {
+  
+  $.fn.carousel=function (options) {
+
+    var $carousel = this;
+
+    var defaults = {
+      itemsVisible: 1,
+    };
+    var settings = $.extend(defaults, options);
+
+    var $slides = $carousel.find('.slides'),
+        $slidesItem = $carousel.find('.slides__item'),
+        $arrowLeft = $carousel.find('.carousel__arrow-left'),
+        $arrowRight = $carousel.find('.carousel__arrow-right');
+
+    var slidesWidth = $slidesItem.length * 100 / settings.itemsVisible + '%',
+        oneSlideWidth = 100 / $slidesItem.length + '%';
+
+    var slidesLeftMargin = parseInt($slides.css('margin-left')),
+        slidesMaxLeftMargin = 100 - parseInt(slidesWidth);
+
+    $slides.css('width', slidesWidth);
+    $slidesItem.css('width', oneSlideWidth);
+
+    $arrowLeft.on('click', toggleSlideLeft);
+    $arrowRight.on('click', toggleSlideRight);
+
+    function toggleSlideLeft(e) {
+      if (slidesLeftMargin < 0) {
+        $slides.animate({
+          marginLeft: '+=100%'
+        }, 200);
+        slidesLeftMargin += 100;
+      }
+      e.preventDefault();
+    }
+    function toggleSlideRight(e) {
+      if (parseInt(slidesLeftMargin) > parseInt(slidesMaxLeftMargin)) {
+        $slides.animate({
+          marginLeft: '-=100%'
+        }, 200);
+        slidesLeftMargin -= 100;
+      }
+      e.preventDefault();
+    }
+
+    return this;
+  };
+})(jQuery);
---
+++ javascript/hw11-12/js/main.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+  $('.carousel').carousel({
+    itemsVisible: 4
+  });
+
+  var employees = $('#employees').html();
+  var employeeForm = [
+                     {
+                       name: 'Иван Иванов',
+                       position: 'junior',
+                       salary: 700,
+                       photo: 'img/huh.png',
+                       characterization: 'Tincidunt integer eu augue augue nunc elit dolor, \
+                                          luctus placerat scelerisque euismod, iaculis eu \
+                                          lacus nunc mi elit, vehicula ut laoreet ac, aliquam \
+                                          sit amet justo nunc tempor, metus vel.'
+                     },
+                     {
+                       name: 'Тарас Тарасов',
+                       position: 'senior',
+                       salary: 2000,
+                       photo: 'img/wow.png',
+                       characterization: 'Tincidunt integer eu augue augue nunc elit dolor, \
+                                          luctus placerat scelerisque euismod, iaculis eu \
+                                          lacus nunc mi elit, vehicula ut laoreet ac, aliquam \
+                                          sit amet justo nunc tempor, metus vel.'
+                     }
+
+  ];
+  
+  var content = tmpl(employees, { data: employeeForm });
+  $('.employees').append(content);
+});
