ca584e5a5713e6983678f3382f5fedf9e14c7001
DemetriosM <dmdonetsk@outlook.com>
2016-05-18 20:05:03 +0300

    добавил hw17-18

---
+++ javascript/hw17-18/src/js/main.js
@@ -0,0 +1,47 @@
+/*-----------------------------Slider-----------------------------*/
+(function($){
+  $(function () {
+      
+    var $carousel = $('.jcarousel');
+
+    $carousel.jcarousel({
+                animation: {
+                  duration: 500
+                }
+              })
+            .jcarouselAutoscroll({
+              interval: 4000,
+              target: '+=1',
+              autostart: true
+    });
+
+    $('.jcarousel__control-prev').jcarouselControl({
+                                    target: '-=1'
+                                  })
+                                .on('click', function () {
+                                  $carousel.jcarouselAutoscroll('stop');
+                                  });
+    $('.jcarousel__control-next').jcarouselControl({
+                                    target: '+=1'
+                                  });
+
+    $('.jcarousel__pagination').jcarouselPagination({
+                                item: function (page) {
+                                  return '<a href="#' + page + '">' + page + '</a>';
+                                }
+                                })
+                              .on('jcarouselpagination:active', 'a', function () {
+                                $(this).addClass('active');
+                                })
+                              .on('jcarouselpagination:inactive', 'a', function () {
+                                $(this).removeClass('active');
+                                })
+                              .jcarouselPagination();
+
+    $('.jcarousel__pagination').find('a').on('click', function(){
+      $carousel.jcarouselAutoscroll('stop');
+    });
+
+  });
+})(jQuery);
+
